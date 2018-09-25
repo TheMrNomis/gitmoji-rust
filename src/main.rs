8c67d845c5d72e804d3eb84aa55b032b89fa8f49
n0m1s <git@homnomnom.fr>
2018-09-25 19:05:42 +0200

    :construction: tests with git2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate git2;
+extern crate dirs;
+
+use git2::Repository;
+use std::path::PathBuf;
+
+fn config_path() -> PathBuf {
+    let mut config_dir = dirs::config_dir().expect("cannot open config dir");
+    config_dir.push("gitmoji-rust");
+
+    return config_dir;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://github.com/carloscuesta/gitmoji/";
+
+    let mut gitmoji_dir = config_path();
+    gitmoji_dir.push("gitmoji");
+
+    let repo = match Repository::clone(url, gitmoji_dir.to_str().expect("error")) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to clone: {}", e),
+    };
 }
