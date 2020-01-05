778ec1efeb9529c221014636be310e61897cee35
n0m1s <git@homnomnom.fr>
2020-01-05 22:48:19 +0100

    ✨ added fuzzy search for gitmoji

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
+extern crate simsearch;
+
 mod settings;
 mod gitmoji;
 
 use settings::Settings;
 use gitmoji::Gitmojis;
+use simsearch::SimSearch;
 
 fn main() {
     let settings = Settings::new(None);
 
     gitmoji::update(&gitmoji::Url::default_github(), &settings.repo_path, &settings.json_path).unwrap();
 
-    let gitmojis = Gitmojis::load(&settings.json_path).unwrap();
-    for gitmoji in gitmojis.gitmojis {
+    let db = Gitmojis::load(&settings.json_path).unwrap();
+    let mut engine: SimSearch<usize> = SimSearch::new();
+
+    for i in 0..db.gitmojis.len() {
+        let gitmoji = &db.gitmojis[i];
+
+        engine.insert(i, &format!("{}: {}", gitmoji.name, gitmoji.description));
+    }
+
+    let results: Vec<usize> = engine.search("lip");
+    for i in results {
+        let gitmoji = &db.gitmojis[i];
         println!("{} - {}", gitmoji.emoji, gitmoji.description);
     }
 }
